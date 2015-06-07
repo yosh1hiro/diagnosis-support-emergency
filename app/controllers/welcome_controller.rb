6464ca76b55bd590bd01d3613e9c4fe7529b618c
yoshihiro asada <yoshihiro0921@gmail.com>
2015-06-08 06:01:11 +0900

    Add searching function in welcome_controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,14 @@
 class WelcomeController < ApplicationController
+  PER = 10
   def index
+    @q = Diesease..page(params[:page]).per(PER).order(:pre_pabability).search(search_params)
+
+    @diseases = @q.result(distinct: true)
   end
+
+  private
+    
+    def search_params
+      params.require(:q).permit(:name_cont, :name_complaint)
+    end
 end
