212a365fadf734ffee50b5f26ca84f59519b3f7d
yoshihiro asada <yoshihiro0921@gmail.com>
2015-06-07 21:25:02 +0900

    Add associations between disease and examination, validation in both file

--- app/models/disease.rb
+++ app/models/disease.rb
@@ -1,2 +1,7 @@
 class Disease < ActiveRecord::Base
+  has_many :examinations, dependent: :destroy
+
+  validates :name, presence: true
+  validates :cheif_complaint, presence: true
+  validates :pre_probability, presence: true
 end
