5930131bd1377bfdb34157f068bd377220b0f872
yoshihiro asada <yoshihiro0921@gmail.com>
2015-06-16 21:08:51 +0900

    Add simplified diagnosis view n controller

--- app/controllers/diagnoses_controller.rb
+++ app/controllers/diagnoses_controller.rb
@@ -1,4 +1,16 @@
 class DiagnosesController < ApplicationController
   def index
+    c2 = Disease.pluck(:cheif_complaint).uniq  # 疾患から主訴を抽出 
+    keys = ["腹痛"]
+    @c2 = Hash[keys.collect.zip(c2)]
+    @diseases = Disease.all.where(params[:cheif_complaint])  # 主訴から病気を抽出
+    @d = Disease.find(1)
+    @examinations = Examination.all.where(disease_id: @d.id)
+    @odds = 0
+  end
+
+  private
+  def diagnoses_params
+    params.require(:diagnoses).permit(:cheif_complaint)
   end
 end
