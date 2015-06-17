f22fc1d3a12e95b971053b2b98b5e559c9895fbb
yoshihiro asada <yoshihiro0921@gmail.com>
2015-06-17 10:30:34 +0900

    Add enum method in model

--- app/controllers/diagnoses_controller.rb
+++ app/controllers/diagnoses_controller.rb
@@ -1,8 +1,6 @@
 class DiagnosesController < ApplicationController
   def index
-    c2 = Disease.pluck(:cheif_complaint).uniq  # 疾患から主訴を抽出 
-    keys = ["腹痛", "失神"]
-    @c2 = Hash[keys.collect.zip(c2)]
+    @c2 = Disease.cheif_complaints  # 疾患から主訴を抽出 
     @diseases = Disease.all.where(params[:cheif_complaint])  # 主訴から病気を抽出
     @d = Disease.find(1)
     @examinations = Examination.all.where(disease_id: @d.id)
--- app/models/disease.rb
+++ app/models/disease.rb
@@ -5,4 +5,12 @@ class Disease < ActiveRecord::Base
   validates :name, presence: true
   validates :cheif_complaint, presence: true
   validates :pre_probability, presence: true
+
+  # setting cheif_complaint
+  # stomachache -> 腹痛, faint -> 2
+  C2 = { stomachache: 1, faint: 2 }
+  enum cheif_complaint: C2
+  def c2_enum
+    C2
+  end
 end
